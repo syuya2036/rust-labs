0b0d8b0d57667fb178fe0ff47e1e28934dc8a9cb
syuya2036 <shuya@yantan.dev>
2026-01-20 02:48:17 +0900

    push_back, pop_backを実装

--- cs/algorithms/src/list/list.rs
+++ cs/algorithms/src/list/list.rs
@@ -49,7 +49,15 @@ impl<T> List<T> {
     }
 
     fn push_back(&mut self, elem: T) {
-        unimplemented!()
+        let new_node = Node::new_shared(elem);
+        if let Some(tail) = self.tail.take() {
+            tail.borrow_mut().next = Some(new_node.clone());
+            new_node.borrow_mut().prev = Some(tail);
+            self.tail = Some(new_node);
+        } else {
+            self.head = Some(new_node.clone());
+            self.tail = Some(new_node);
+        }
     }
 
     fn pop_front(&mut self) -> Option<T> {
@@ -67,7 +75,18 @@ impl<T> List<T> {
     }
 
     fn pop_back(&mut self) -> Option<T> {
-        unimplemented!()
+        if let Some(tail) = self.tail.take() {
+            if let Some(new_tail) = tail.borrow_mut().prev.take() {
+                new_tail.borrow_mut().next = None;
+                self.tail = Some(new_tail);
+            } else {
+                // 要素1個
+                self.head.take();
+            }
+            Some(Rc::try_unwrap(tail).ok().unwrap().into_inner().elem)
+        } else {
+            None
+        }
     }
 
     fn is_empty(&self) -> bool {
@@ -122,5 +141,41 @@ mod test {
         for i in 0..lst.len() {
             assert_eq!(lst.pop_front().unwrap(), elems[elems.len() - i - 1])
         }
+    }
+
+    #[test]
+    fn test_back() {
+        let mut lst = List::new();
+        let elems = vec![1, 1, 2, 3, 5, 8, 13];
+        for e in elems.clone() {
+            lst.push_back(e);
+        }
+
+        for i in 0..elems.len() {
+            assert_eq!(lst.pop_back().unwrap(), elems[elems.len() - i - 1]);
+        }
+    }
+
+    #[test]
+    fn test_mixed_ops() {
+        let mut lst = List::new();
+
+        lst.push_front(2); // [2]
+        lst.push_front(1); // [1,2]
+        assert_eq!(lst.len(), 2);
+        lst.push_back(3); // [1,2,3]
+        lst.push_back(4); // [1,2,3,4]
+        assert_eq!(lst.len(), 4);
+        assert!(!lst.is_empty());
+
+        assert_eq!(lst.pop_front(), Some(1)); // [2,3,4]
+        assert_eq!(lst.pop_back(), Some(4)); // [2,3]
+        assert_eq!(lst.len(), 2);
+        assert_eq!(lst.pop_back(), Some(3)); // [2]
+        assert_eq!(lst.pop_front(), Some(2)); // []
+
+        assert!(lst.is_empty());
+        assert_eq!(lst.pop_front(), None);
+        assert_eq!(lst.pop_back(), None);
     }
 }
